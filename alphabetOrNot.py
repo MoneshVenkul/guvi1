a332d585bfb4813a861a14bf2247fe884a4dfd6e
MoneshVenkul <48666307+MoneshVenkul@users.noreply.github.com>
2019-03-20 13:04:30 +0530

    Update alphabetOrNot.py

--- alphabetOrNot.py
+++ alphabetOrNot.py
@@ -1,7 +1,7 @@
-l1 = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+a1 = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
 ch = str(input())
-if (ch in l1):
+if (ch in a1):
     print ("Alphabet")
 else:
     print("No")
